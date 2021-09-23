bbf6e944b9352050d73df3d71a5f197ca78944bc
omerffaruk <farukyamak11044@gmail.com>
2021-09-23 12:17:10 +0100

    feat: html structured, initially styled the page, built the page by fetching data from the api

--- script.js
+++ script.js
@@ -1,81 +1,88 @@
 //You can edit ALL of the code here
-const allEpisodes = getAllEpisodes();   // allEpisodes is storing an array of episode objects   
+// <---- These are variables that store HTML elements that needed ---->
+const api = "https://api.tvmaze.com/shows/82/episodes";
 const rootElem = document.getElementById("root");   // div that wraps all the epContainer divs
 const epSearch = document.getElementById("epSearch");   // search input field
 const totalDisplayingEpsP = document.getElementById("totalDisplayingEps"); 
 const select = document.getElementById("epDropdown");
 
-function setup() {    // function that will be called when the page is loaded
-  makePageForEpisodes(allEpisodes);   // making a call to makePageForEpisodes which creates episode pages
-  totalDisplayingEpsP.textContent = `Displaying all ${allEpisodes.length} episodes`;
+async function getEpsWithFetchAndBuildThePage () {  //builds the page with ep cards and returns json in array format
+  const promise = await fetch(api);
+  const json = await promise.json();
+  makePageForEpisodes(json);
+  createDropDownSelectEpMenu(json);
+ 
+  function filterEpisodes(word) {  // only return episodes that include the searched input value in their summary or name (for search) 
+    return json.filter((ep) => {
+      return (
+        ep.name.toLowerCase().includes(word) ||
+        ep.summary.toLowerCase().includes(word)
+      );
+    });
+  }
+  totalDisplayingEpsP.textContent = `Displaying all ${json.length} episodes`;
+
+  
+  function render(word = "") { // render the page according the search input value
+    rootElem.innerHTML = "";
+    word = cleanUpWord(word);
+    const filtered = filterEpisodes(word);
+    totalDisplayingEpsP.innerText = `Displaying ${filtered.length}/${json.length} episodes.`;
+    makePageForEpisodes(filtered);
+  }
+
+  epSearch.addEventListener("input", () => {
+    render(epSearch.value);
+  });
+  return json;
 }
 
-function makePageForEpisodes(episodeList) {   // main function: this will create div for each ep, fill the select dropdown with eps options
-  function formatEp (num) {   // format the number suitable for Season - Episode format
-    const str = num.toString();
-    return str.padStart(2, "0");
-  }
-  for(let i = 0; i < episodeList.length; i++) {   // render all episodes 
+function makePageForEpisodes(episodeList) {  // this function creates div for each ep and fill their content
+  for(let i = 0; i < episodeList.length; i++) {    
     const ep = episodeList[i]; 
     const epContainerDiv = document.createElement("div");   // create container div for each episode
     epContainerDiv.setAttribute("class", "epContainer");
     epContainerDiv.setAttribute("id", `div${i+1}`);
-    rootElem.appendChild(epContainerDiv);
     const a = document.createElement("a");    // turn div content into a link 
+    rootElem.appendChild(a);
     a.setAttribute("href", ep.url)
     a.setAttribute("target", "_blank");
     a.setAttribute("class", "epLink");
-    epContainerDiv.appendChild(a);
+    a.appendChild(epContainerDiv);
     const epHeader = document.createElement("h2");    // create header for each episode
     epHeader.setAttribute("class", "epHeader");
     epHeader.setAttribute("id", `epHeader${i+1}`);
     epHeader.innerText = `${ep.name} - S${formatEp(ep.season)}E${formatEp(ep.number)}`;
-    a.appendChild(epHeader);
+    epContainerDiv.appendChild(epHeader);
     const epImage = document.createElement("img");    // create img element for ep image
     epImage.setAttribute("class", "epImage");
     epImage.setAttribute("id", `epImage${i+1}`);
     epImage.src = ep.image.medium;
-    a.appendChild(epImage);
+    epContainerDiv.appendChild(epImage);
     const epSummaryP = document.createElement("p");   // create p element for ep summary
     epSummaryP.setAttribute("class", "epSummaryP");
     epSummaryP.setAttribute("id", `epSummaryP${i+1}`);
     epSummaryP.innerHTML = `${ep.summary}`;
-    a.appendChild(epSummaryP);
-    const option = document.createElement("option");    // create option element for each ep and fill the select dropdown
-    option.value = `S${formatEp(ep.season)}E${formatEp(ep.number)} - ${ep.name}`;
-    select.add(option);
-    option.innerHTML = `<a href = ${ep.url} target = _blank>S${formatEp(ep.season)}E${formatEp(ep.number)} - ${ep.name}</a>`;
+    epContainerDiv.appendChild(epSummaryP);
   }
 }
 
-function cleanUpWord (word) {   // format the input value (toLowerCase and trim)
-  return word.trim().toLowerCase();
+function formatEp(num) {  // format the number suitable for Season - Episode format
+  const str = num.toString();
+  return str.padStart(2, "0");
 }
 
-function filterEpisodes (word) {    // only return episodes that include the searched input value in their summary or name
-  return allEpisodes.filter((ep) => {
-    return ep.name.toLowerCase().includes(word) || ep.summary.toLowerCase().includes(word);
+function createDropDownSelectEpMenu (episodeList) {  // creates drop down select menu with each option a link to the episode
+  episodeList.forEach((ep) => {
+     const option = document.createElement("option"); // create option element for each ep and fill the select dropdown
+     option.value = `${ep.url}`;
+     select.add(option);
+     option.innerHTML = `S${formatEp(ep.season)}E${formatEp(ep.number)} - ${ep.name}`;
   })
 }
 
-function render (word = "") {   // render the page according the search input value
-  rootElem.innerHTML = "";
-  word = cleanUpWord(word);
-  const filtered = filterEpisodes(word);
-  totalDisplayingEpsP.innerText = `Displaying ${filtered.length}/${allEpisodes.length} episodes.`;
-  makePageForEpisodes(filtered);
+function cleanUpWord (word) {   // format the input value (for the search bar) (toLowerCase and trim)
+  return word.trim().toLowerCase();
 }
 
-epSearch.addEventListener("input", () => {
-  render(epSearch.value);
-})
-
-// function renderFooter () {
-//   const footer = document.createElement("footer");
-//   footer.innerHTML = 'The data shown in this page is provided by <a href="https://www.tvmaze.com/"target="_blank">TVMaze.com</a>';
-//   wrapper.appendChild(footer);
-// }
-
-window.onload = setup;
-// when the onload event happens -> call the setup function 
-// window is a global object in the browser      
+window.onload = getEpsWithFetchAndBuildThePage;
